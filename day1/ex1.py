44b952c4ea2e705d4877ff20b17c71e184a4b852
Loan <loanbillonnet@gmail.Com>
2021-12-01 14:05:54 +0100

    day1 ex1 - optimization

--- day1/ex1.py
+++ day1/ex1.py
@@ -1,9 +1,9 @@
 file = open("input.txt")
-data = file.readlines()
+data = list(map(int, file.readlines()))
 result = 0
 
 for i in range(len(data)):
     if i != 0:
-        if int(data[i]) - int(data[i - 1]) > 0:
+        if data[i] - data[i - 1] > 0:
             result += 1
 print(result)
