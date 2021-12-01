641aed765ca0fb06df10a4653faec9da1d445237
Loan <loanbillonnet@gmail.Com>
2021-12-01 14:00:36 +0100

    day1 ex2 - optimization

--- day1/ex2.py
+++ day1/ex2.py
@@ -1,12 +1,12 @@
 file = open("input.txt")
-data = file.readlines()
+data = list(map(int, file.readlines()))
 
 listResult = []
 result = 0
 
 for i in range(len(data)):
     if (i + 2) < len(data):
-        listResult.append(int(data[i]) + int(data[i + 1]) + int(data[i + 2]))
+        listResult.append(data[i] + data[i + 1] + data[i + 2])
 
 for i in range(len(listResult)):
     if i != 0:
