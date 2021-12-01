268d84434c28d84571e96cd7281c3826ff0e8dff
Loan <loanbillonnet@gmail.Com>
2021-12-01 13:46:30 +0100

    day1 ex2

--- day1/ex2.py
+++ day1/ex2.py
@@ -1,3 +1,16 @@
 file = open("input.txt")
 data = file.readlines()
 
+listResult = []
+result = 0
+
+for i in range(len(data)):
+    if (i + 2) < len(data):
+        listResult.append(int(data[i]) + int(data[i + 1]) + int(data[i + 2]))
+
+for i in range(len(listResult)):
+    if i != 0:
+        if listResult[i] - listResult[i - 1] > 0:
+            result += 1
+
+print(result)
